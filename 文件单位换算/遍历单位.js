0e0f49939c4dfae201554e0a6be68832f3f80d19
dubin.0116 <dubin.0116@bytedance.com>
2024-06-04 10:59:24 +0800

    补充

--- 文件单位换算/遍历单位.js
+++ 文件单位换算/遍历单位.js
@@ -1,3 +1,4 @@
+//本题来源于：https://juejin.cn/post/7360528073631318027
 function handler(kb) {
   const unit = ["B","KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   let index = 0;
@@ -9,3 +10,23 @@ function handler(kb) {
 }
 
 handler(1024*1024+1)
+
+/*
+比较low的写法
+function formatSizeUnits(kb) {
+    var result = '';
+
+    if (kb < 1024) {
+        result = kb + ' KB';
+    } else if (kb < 1024 * 1024) {
+        result = (kb / 1024).toFixed(2) + ' MB';
+    } else if (kb < 1024 * 1024 * 1024) {
+        result = (kb / 1024 / 1024).toFixed(2) + ' GB';
+    } else {
+        result = (kb / 1024 / 1024 / 1024).toFixed(2) + ' TB';
+    }
+
+    return result;
+}
+*/
+
